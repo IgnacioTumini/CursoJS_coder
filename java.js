7455fe7fbd4423beccf2f09db848bf26f19c447f
ignacio <ignacio.tumini@hotmail.com>
2023-02-03 12:32:23 -0300

    dom intermedio, falta storage

--- java.js
+++ java.js
@@ -1,9 +1,6 @@
 // CALCULO INTERES //
 function calcular_interes(monto, cuotas) {
   let interes;
-  //const interesTresCuotas = parseFloat(0.15);
-  //const interesSeisCuotas = parseFloat(0.3);
-  //const interesDoceCuotas = parseFloat(0.6);
 
   if (cuotas == 3) {
     interes = monto * 0.15;
@@ -15,16 +12,23 @@ function calcular_interes(monto, cuotas) {
   } else if (cuotas == 12) {
     interes = monto * 0.6;
     return interes;
+  } else if (cuotas == 18) {
+    interes = monto * 1.2;
+    return interes;
+  } else if (cuotas == 24) {
+    interes = monto * 2.4;
+    return interes;
   } else return 0;
 }
 // FIN CALCULO //
-// CALCULO TOTAL A PAGAR //
 
 // OBJETO PRESTAMO //
 
 class Prestamo {
-  constructor(cliente, monto, cuotas, monto_final, id) {
-    this.cliente = cliente;
+  constructor(nombre, apellido, mail, monto, cuotas, monto_final, id) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.mail = mail;
     this.monto = monto;
     this.cuotas = cuotas;
     this.monto_final = monto_final;
@@ -32,7 +36,9 @@ class Prestamo {
   }
   get_datos() {
     console.log("<----------------->");
-    console.log("Cliente: ", this.cliente);
+    console.log("Nombre: ", this.nombre);
+    console.log("Apellido: ", this.apellido);
+    console.log("Mail: ", this.mail);
     console.log("Prestamo pedido: $", this.monto);
     console.log("Cantidad de cuotas a pagar: ", this.cuotas);
     console.log("Prestamo a devolver: $", this.monto_final);
@@ -46,74 +52,53 @@ class Prestamo {
 let lista_prestamos = [];
 
 // INTERACCION CON EL CLIENTE //
-let fin;
-let id = 1;
-alert("Bienvenido a TumiPresta");
-while (fin != 2) {
-  let cliente = prompt("Ingrese su nombre y apellido");
-  let monto = parseFloat(
-    prompt("Ingrese el prestamo a solicitar entre $5000 y $60000")
-  );
 
-  while (monto > 60000 || monto < 5000) {
-    alert(
-      "Prestamo fuera de terminos por favor ingrese dentro de los solicitado"
-    );
-    monto = parseFloat(
-      prompt("Ingrese el prestamo a solicitar entre $5000 y $60000")
-    );
-  }
-  let cuotas = parseInt(prompt("ingrese las cuotas, 3,6 o 12"));
-  while (cuotas != 3 && cuotas != 6 && cuotas != 12) {
-    alert(
-      "Cuotas fuera de terminos por favor ingrese dentro de los solicitado"
-    );
-    cuotas = parseInt(prompt("ingrese las cuotas, 3,6 o 12"));
-  }
+let id = 1;
+
+function toma_datos() {
+  let nombre = document.getElementById("exampleInputPassword1").value;
+  let apellido = document.getElementById("exampleInputPassword6").value;
+  let mail = document.getElementById("exampleInputEmail1").value;
+  let monto = parseInt(document.getElementById("exampleInputPassword4").value);
+  let cuotas = parseInt(document.getElementById("select_cuotas").value);
+
   monto_final = monto + calcular_interes(monto, cuotas);
 
-  alert(
-    "Su prestamo a pagar seria $" +
-      (monto + calcular_interes(monto, cuotas)) +
-      ", en cuotas de $" +
-      (monto + calcular_interes(monto, cuotas)) / cuotas
+  let nuevo_prestamo = new Prestamo(
+    nombre,
+    apellido,
+    mail,
+    monto,
+    cuotas,
+    monto_final,
+    id
   );
-  fin = parseInt(
-    prompt("si desea otro prestamos ponga 1, si desea salir ponga 2")
-  );
-
-  let nuevo_prestamo = new Prestamo(cliente, monto, cuotas, monto_final, id);
   lista_prestamos.push(nuevo_prestamo);
   id += 1;
+
+  // INICIO RENDER DE PRESTAMOS //
+
+  for (let nuevo_prestamo of lista_prestamos) {
+    console.clear;
+    nuevo_prestamo.get_datos();
+  }
+  // FIN RENDER //
 }
-
-alert("Gracias por confiar en nosotros!");
-// FIN INTERACCION //
-
-// INICIO RENDER DE PRESTAMOS //
-for (let nuevo_prestamo of lista_prestamos) {
-  nuevo_prestamo.get_datos();
-}
-// FIN RENDER //
-
-// busqueda de prestamos //
-
 function buscar_id(Prestamo) {
   return Prestamo.id == busqueda_prestamo;
 }
 
-fin = parseInt(prompt("Para buscar algun prestamo ingrese 1, sino 2"));
-while (fin != 2) {
-  busqueda_prestamo = parseInt(prompt("Ingrese el id del prestamo"));
-  let resultado_busqueda = lista_prestamos.find(buscar_id);
-  if (resultado_busqueda != undefined) {
-    console.log("el prestamo buscado se encontro", resultado_busqueda);
-  } else {
-    alert("no se encontro el prestamo");
-  }
+//fin = parseInt(prompt("Para buscar algun prestamo ingrese 1, sino 2"));
+//while (fin != 2) {
+//  busqueda_prestamo = parseInt(prompt("Ingrese el id del prestamo"));
+//  let resultado_busqueda = lista_prestamos.find(buscar_id);
+//  if (resultado_busqueda != undefined) {
+//    console.log("el prestamo buscado se encontro", resultado_busqueda);
+//  } else {
+//alert("no se encontro el prestamo");
+//}
 
-  fin = parseInt(prompt("Para buscar algun prestamo mas ingrese 1, sino 2"));
-}
+//  fin = parseInt(prompt("Para buscar algun prestamo mas ingrese 1, sino 2"));
 
 //let resultado_busqueda = lista_prestamos.find(buscar_id);
 //console.log("el prestamo buscado se encontro", resultado_busqueda);
